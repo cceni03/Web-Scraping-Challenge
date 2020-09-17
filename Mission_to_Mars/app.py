e80adec54b4c5ff2274dc86362c471cb011a1600
cceni03 <czc5230.cc@gmail.com>
2020-09-17 00:38:41 -0400

    Update

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import Scrape_Mars
+import scrape_mars
 
 app = Flask(__name__)
 
@@ -14,8 +14,8 @@ def index():
 
 @app.route("/scrape")
 def scrape():
-    mars_data = Scrape_Mars.scrape()
-    mongo.db_mars_dict.update({}, mars_info, upsert=True)
+    mars_data = scrape_mars.scrape()
+    mongo.db.mars_dict.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
 
 if __name__ == "__main__":
